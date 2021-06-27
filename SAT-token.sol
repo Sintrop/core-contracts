ae95ba70b8191baf6565b36ae58e1fc43f30b510
André Ravagnani <79549809+rav-andre@users.noreply.github.com>
2021-06-27 12:37:24 -0300

    Create SAT-token.sol

---
+++ SAT-token.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.0;
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract SATtoken {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
