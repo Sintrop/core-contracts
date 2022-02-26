b54932e44600a2289a07d23319ac72ff7630c583
Everson Silva <eversonsilva9799@gmail.com>
2022-02-26 10:03:02 -0300

    feat: use zepellin ownable

--- contracts/DeveloperPool.sol
+++ contracts/DeveloperPool.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <=0.9.0;
 
-// import "@openzeppelin/contracts/access/Ownable.sol";
 import "./PoolInterface.sol";
 
 import "./SatTokenInterface.sol";
-import "./Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "./SafeMath.sol";
 
