ac8a76f4fc44f983b50137b3212288f7256ad3af
Everson Silva <eversonsilva9799@gmail.com>
2022-04-02 06:59:30 -0300

    test: add specs to category vote/unvote isapool

--- test/CategoryContract.test.js
+++ test/CategoryContract.test.js
@@ -1,7 +1,11 @@
 const CategoryContract = artifacts.require("CategoryContract");
+const SatToken = artifacts.require("SatToken");
+const IsaPool = artifacts.require("IsaPool");
 
 contract('CategoryContract', (accounts) => {
   let instance;
+  let satToken;
+  let isaPool;
   let [msgSender, user1Address, user2Address] = accounts;
 
   const addCategory = async (name) => {
@@ -16,10 +20,24 @@ contract('CategoryContract', (accounts) => {
     )
   }
 
-  beforeEach(async () => {
-    instance = await CategoryContract.new();
-  })
+  const transferTokensTo = async (userAddress, tokens) => {
+    await satToken.transfer(userAddress, tokens);
+  }
 
+  const balanceOf = async (userAddress) => {
+    return await satToken.balanceOf(userAddress);
+  }
+
+  beforeEach(async () => {
+    satToken = await SatToken.new("1500000000000000000000000000");
+    isaPool = await IsaPool.new(satToken.address);
+
+    await satToken.addContractPool(isaPool.address, "0")
+    
+    instance = await CategoryContract.new(isaPool.address);
+    await isaPool.changeAllowedCaller(instance.address);
+  })
+  
   it("should create category", async () => {
     await addCategory("Solo");
     const categories = await instance.getCategories();
@@ -76,15 +94,6 @@ contract('CategoryContract', (accounts) => {
     assert.equal(parseInt(categories[0].votesCount), 0)
   })
 
-  it("should increment vote in 1 when call vote function", async () => {
-    await addCategory("Solo");
-    await instance.vote(1, 0);
-
-    const category = await instance.categories(1);
-
-    assert.equal(category.votesCount, 1);
-  })
-
   it("when user try vote and category dont exists should return error message", async () => {
     await instance.vote(1, 0)
     .then(assert.fail)
@@ -95,91 +104,138 @@ contract('CategoryContract', (accounts) => {
 
   it("should vote and send tokens only to the passed id", async () => {
     await addCategory("Solo");
-    await instance.vote(1, 100);
+    await instance.vote(1, "100000000000000000000");
 
     const votes1 = await instance.votes(1);
     const votes2 = await instance.votes(2);
 
-    assert.equal(votes1, 100);
+    assert.equal(votes1, "100000000000000000000");
     assert.equal(votes2, 0);
   })
 
   it("should increment 100 tokens when call vote function and send 100 tokens", async () => {
     await addCategory("Solo");
-    await instance.vote(1, 100);
+    await instance.vote(1, "100000000000000000000");
 
     const votes = await instance.votes(1);
 
-    assert.equal(votes, 100);
+    assert.equal(votes, "100000000000000000000");
   })
 
   it("should have 150 tokens when call vote function and send 100 tokens and already have 50 tokens", async () => {
     await addCategory("Solo");
-    await instance.vote(1, 50);
-    await instance.vote(1, 100);
+    await instance.vote(1, "50000000000000000000");
+    await instance.vote(1, "100000000000000000000");
 
     const votes = await instance.votes(1);
 
-    assert.equal(votes, 150);
-  })
-
-  it("should increment 0 tokens when call vote function and dont send tokens", async () => {
-    await addCategory("Solo");
-    await instance.vote(1, 0);
-
-    const votes = await instance.votes(1);
-
-    assert.equal(votes, 0);
+    assert.equal(votes, "150000000000000000000");
   })
 
   it("should set amount of tokens that the user voted to category id when vote", async () => {
     await addCategory("Solo 1");
     await addCategory("Solo 2");
-    await instance.vote(1, 100);
-    await instance.vote(2, 50);
+    await instance.vote(1, "100000000000000000000");
+    await instance.vote(2, "50000000000000000000");
 
-    const votes1 = await instance.voted(msgSender, 1);
-    const votes2 = await instance.voted(msgSender, 2);
-    const votes3 = await instance.voted(msgSender, 3);
+    const voted1 = await instance.voted(msgSender, 1);
+    const voted2 = await instance.voted(msgSender, 2);
+    const voted3 = await instance.voted(msgSender, 3);
 
-    assert.equal(votes1, 100);
-    assert.equal(votes2, 50);
-    assert.equal(votes3, 0);
+    assert.equal(voted1, "100000000000000000000");
+    assert.equal(voted2, "50000000000000000000");
+    assert.equal(voted3, 0);
   })
 
   it("should start with voted zero categories", async () => {
     await addCategory("Solo 1");
     await addCategory("Solo 2");
 
-    const votes1 = await instance.voted(msgSender, 1);
-    const votes2 = await instance.voted(msgSender, 0);
+    const voted1 = await instance.voted(msgSender, 1);
+    const voted2 = await instance.voted(msgSender, 0);
 
-    assert.equal(votes1, 0);
-    assert.equal(votes2, 0);
+    assert.equal(voted1, 0);
+    assert.equal(voted2, 0);
   })
 
   it("should increment vote of users in same category", async () => {
     await addCategory("Solo 1");
 
-    await instance.vote(1, 100, { from: user1Address });
-    await instance.vote(1, 100, { from: user2Address });
+    await transferTokensTo(user1Address, "50000000000000000000000");
+    await transferTokensTo(user2Address, "50000000000000000000000");
+
+    await instance.vote(1, "100000000000000000000", { from: user1Address });
+    await instance.vote(1, "100000000000000000000", { from: user2Address });
 
     const votes = await instance.votes(1);
 
-    assert.equal(votes, 200);
+    assert.equal(votes, "200000000000000000000");
   })
 
   it("when more than one user voted in one category each user must have your tokens part", async () => {
     await addCategory("Solo 1");
 
-    await instance.vote(1, 100, { from: user1Address });
-    await instance.vote(1, 100, { from: user2Address });
+    await transferTokensTo(user1Address, "50000000000000000000000");
+    await transferTokensTo(user2Address, "50000000000000000000000");
+
+    await instance.vote(1, "100000000000000000000", { from: user1Address });
+    await instance.vote(1, "100000000000000000000", { from: user2Address });
 
     const votes1 = await instance.voted(user1Address, 1);
     const votes2 = await instance.voted(user2Address, 1);
 
-    assert.equal(votes1, 100);
-    assert.equal(votes2, 100);
+    assert.equal(votes1, "100000000000000000000");
+    assert.equal(votes2, "100000000000000000000");
+  })
+
+  it("should return error message when try vote and dont has SAC Tokens", async () => {
+    await addCategory("Solo");
+    await instance.vote(1, 0, { from: user1Address })
+    .then(assert.fail)
+    .catch((error) => {
+      assert.equal(error.reason, "You don't have tokens to vote")
+    })
+  })
+
+  it("should return error message when try vote and has tokens but dont send any", async () => {
+    await addCategory("Solo");
+    await instance.vote(1, 0)
+    .then(assert.fail)
+    .catch((error) => {
+      assert.equal(error.reason, "You must send at least 1 SAC Token")
+    })
+  })
+
+  it("should subtract tokens from user after vote in a category", async () => {
+    await addCategory("Solo");
+    await transferTokensTo(user1Address, "50000000000000000000000");
+    await instance.vote(1, "1000000000000000000000", { from: user1Address });
+
+    const balanceOf = await isaPool.balanceOf(user1Address);
+
+    assert.equal(balanceOf, "49000000000000000000000");
+  })  
+  
+  it("should add tokens to isa pool after vote in a category", async () => {
+    await addCategory("Solo");
+    await transferTokensTo(user1Address, "50000000000000000000000");
+    await instance.vote(1, "1000000000000000000000", { from: user1Address });
+
+    const balance = await isaPool.balance();
+
+    assert.equal(balance, "1000000000000000000000");
+  })
+
+  it("should increment tokens in isa pool after many user votes", async () => {
+    await addCategory("Solo");
+    await transferTokensTo(user1Address, "50000000000000000000000");
+    await transferTokensTo(user2Address, "50000000000000000000000");
+    await instance.vote(1, "1000000000000000000000", { from: user1Address });
+    await instance.vote(1, "5000000000000000000000", { from: user2Address });
+
+    const balance = await isaPool.balance();
+
+    assert.equal(balance, "6000000000000000000000");
   })
 
   it("when user try unvote and category dont exists should return error message", async () => {
@@ -188,48 +244,66 @@ contract('CategoryContract', (accounts) => {
     .catch((error) => {
       assert.equal(error.reason, "This category don't exists")
     })
-  })   
+  })
   
-  it("should return zero tokens when user unvote in a category he didn't vote for", async () => {
-    await addCategory("Solo 1");
+  it("should decrement votesCount in 1 when unvote with success", async () => {
+    await addCategory("Solo");
+    await transferTokensTo(user1Address, "50000000000000000000000");
 
-    const unvote = await instance.unvote.call(1);
+    await instance.vote(1, "100000000000000000000", { from: user1Address });
+    await instance.vote(1, "100000000000000000000");
+    await instance.unvote(1);
 
-    assert.equal(unvote, 0);
-  })  
+    const category = await instance.categories(1);
 
-  it("dont should remove votes when user try unvote and dont voted yet", async () => {
-    await addCategory("Solo 1");
-
-    await instance.vote(1, 100);
-
-    const unvote = await instance.unvote.call(1, { from: user1Address });
-    const votes = await instance.votes(1);
-
-    assert.equal(unvote, 0);
-    assert.equal(votes, 100);
+    assert.equal(category.votesCount, 1);
   }) 
 
-  it("should return 100 tokens when user unvote and already voted with 100 tokens", async () => {
+  it("should return error message when user try unvote and dont voted yet", async () => {
     await addCategory("Solo 1");
 
-    await instance.vote(1, 100);
-    const unvote = await instance.unvote.call(1);
+    await instance.vote(1, "100000000000000000000");
 
-    assert.equal(unvote, 100);
-  })
+    await instance.unvote(1, { from: user1Address })
+    .then(assert.fail)
+    .catch((error) => {
+      assert.equal(error.reason, "You don't voted to this category")
+    })
+  }) 
 
-  it("when a user unvoted with success should remove votes from category and from user voted mapping", async () => {
+  it("when a user unvoted with success should remove votes/tokens from voted mapping", async () => {
     await addCategory("Solo 1");
 
-    await instance.vote(1, 5000);
-    await instance.vote(1, 150, { from: user1Address });
-
-    await instance.unvote(1);
-    const votes = await instance.votes(1);
+    await instance.vote(1, "100000000000000000000");
+    const unvote = await instance.unvote(1);
     const voted = await instance.voted(msgSender, 1)
 
-    assert.equal(votes, 150);
     assert.equal(voted, 0);
   })
+
+  it("when a user unvoted with success should remove votes/tokens from category", async () => {
+    await addCategory("Solo 1");
+
+    await instance.vote(1, "100000000000000000000");
+    await instance.unvote(1);
+    const votes = await instance.votes(1);
+
+    assert.equal(votes, 0);
+  })
+
+  it("when a user unvoted in a category your tokens should allowed back", async () => {
+    await addCategory("Solo 1");
+    await addCategory("Solo 2");
+
+    await transferTokensTo(user1Address, "50000000000000000000000");
+
+    await instance.vote(1, "1000000000000000000000", { from: user1Address });
+    await instance.vote(2, "5000000000000000000000", { from: user1Address });
+
+    await instance.unvote(1, { from: user1Address });
+
+    const allowance = await isaPool.allowance({ from: user1Address });
+
+    assert.equal(allowance, "1000000000000000000000");
+  }) 
 })
