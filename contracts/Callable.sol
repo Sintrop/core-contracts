fffbe8c36a827e3b4a3a2d2fc055e95f18769ffc
Everson Silva <eversonsilva9799@gmail.com>
2022-07-13 21:44:34 -0300

    refact: allowed caller to plural

--- contracts/Callable.sol
+++ contracts/Callable.sol
@@ -5,14 +5,18 @@ pragma solidity >=0.7.0 <=0.9.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Callable is Ownable {
-  mapping(address => bool) public allowedCaller;
+  mapping(address => bool) public allowedCallers;
 
   function newAllowedCaller(address allowed) public onlyOwner {
-    allowedCaller[allowed] = true;
+    allowedCallers[allowed] = true;
+  }
+
+  function isAllowedCaller(address caller) public view returns (bool) {
+    return allowedCallers[caller];
   }
 
   modifier mustBeAllowedCaller() {
-    require(allowedCaller[msg.sender], "Not allowed caller");
+    require(allowedCallers[msg.sender], "Not allowed caller");
     _;
   }
 }
